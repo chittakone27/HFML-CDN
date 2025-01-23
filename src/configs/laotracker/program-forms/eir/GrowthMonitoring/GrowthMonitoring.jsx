5ac476c6694bdaa925e74ce70912c9692dd0c102
Uoc Trinh <36979461+batisuoc@users.noreply.github.com>
2025-01-23 16:56:19 +0700

    EIR Growth Monitor - Init

--- src/configs/laotracker/program-forms/eir/GrowthMonitoring/GrowthMonitoring.jsx
+++ src/configs/laotracker/program-forms/eir/GrowthMonitoring/GrowthMonitoring.jsx
@@ -1,34 +1,94 @@
 import { Box, Table, TableBody } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { useShallow } from "zustand/react/shallow";
+import useCurrentEvent from "@/ui/TrackerCapture/EventForm/useCurrentEvent";
+import useTrackerCaptureStore from "@/state/trackerCapture";
+import useSelectionStore from "@/state/selection";
 
-import { withEventDate, RowMapper, SectionCollapse } from "@/configs/lao/program-forms/common/tracker";
+import {
+  withEventDate,
+  RowMapper,
+  SectionCollapse
+} from "@/configs/lao/program-forms/common/tracker";
+//
+import { GROWTH_MONITOR_ID, CHILD_NUTRI_STATUS_SECTION_ID } from "./const";
+//
+import "./GrowthMonitoring.css";
 import "../eir.css";
 
 const GrowthMonitoring = () => {
   const { t } = useTranslation();
+  const { program } = useSelectionStore(
+    useShallow((state) => ({
+      program: state.program
+    }))
+  );
+  const growthMonitorStage = program.programStages.find(
+    (progState) => progState.id === GROWTH_MONITOR_ID
+  );
+  const { programStageSections } = growthMonitorStage;
+  // console.log(programStageSections);
 
   return (
     <Box className="eir-form">
-      <SectionCollapse title={t("Growth monitoring details")} disabledCollapse>
-        <Table>
-          <TableBody>
-            <RowMapper rows={section1Configs} tableName={"Growth monitoring details"} context="event" />
-          </TableBody>
-        </Table>
-      </SectionCollapse>
-      <SectionCollapse title={t("Vit A & Deworming")} disabledCollapse sx={{ mt: 1 }}>
-        <Table>
-          <TableBody>
-            <RowMapper rows={section2Configs} tableName={"Vit A & Deworming"} context="event" />
-          </TableBody>
-        </Table>
-      </SectionCollapse>
+      {programStageSections.map((pss) => {
+        const tableConfigs = pss.dataElements.map((de) => [{ id: de.id }]);
+        if (pss.id === CHILD_NUTRI_STATUS_SECTION_ID) {
+          return (
+            <SectionCollapse
+              key={pss.id}
+              title={t(pss.displayName)}
+              disabledCollapse
+              sx={{
+                marginBottom: "10px"
+              }}
+            >
+              <div className="child-nutri-container">
+                <SectionCollapse
+                  title={"AchiAcho"}
+                  sx={{ mt: 0.5 }}
+                  disabledCollapse
+                >
+                  <Table>
+                    <TableBody>
+                      sdsdsdsdsdsds
+                      {/* <RowMapper
+                rows={tableConfigs}
+                tableName={"Growth monitoring details"}
+                context="event"
+              /> */}
+                    </TableBody>
+                  </Table>
+                </SectionCollapse>
+              </div>
+            </SectionCollapse>
+          );
+        } else {
+          return (
+            <SectionCollapse
+              key={pss.id}
+              title={t(pss.displayName)}
+              disabledCollapse
+              sx={{
+                marginBottom: "10px"
+              }}
+            >
+              <Table>
+                <TableBody>
+                  <RowMapper
+                    rows={tableConfigs}
+                    tableName={"Growth monitoring details"}
+                    context="event"
+                  />
+                </TableBody>
+              </Table>
+            </SectionCollapse>
+          );
+        }
+      })}
     </Box>
   );
 };
-
-const section1Configs = [[{ id: "DxOqZZgVQhF" }], [{ id: "MV1yoC7BfnG" }], [{ id: "xvE2z6W3wYh" }], [{ id: "acQoZnFeVYZ" }], [{ id: "fwerjuyn3QC" }]];
-
-const section2Configs = [[{ id: "GVHTqqwolWD" }], [{ id: "VUn6z5bss2H" }], [{ id: "DzNWdRvRB11" }]];
 
 export default withEventDate(GrowthMonitoring, "eventDate");
---
+++ src/configs/laotracker/program-forms/eir/GrowthMonitoring/const.js
@@ -0,0 +1,2 @@
+export const GROWTH_MONITOR_ID = "ibCNFZaNuBc";
+export const CHILD_NUTRI_STATUS_SECTION_ID = "EaOceques9f";
