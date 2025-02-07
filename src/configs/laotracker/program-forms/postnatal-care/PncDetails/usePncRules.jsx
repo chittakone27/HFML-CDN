b54c86e89456bd2c3bb127c1e05579b423856d5d
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2025-02-07 11:56:04 +0700

    enable service type PNC

--- src/configs/laotracker/program-forms/postnatal-care/PncDetails/usePncRules.jsx
+++ src/configs/laotracker/program-forms/postnatal-care/PncDetails/usePncRules.jsx
@@ -31,7 +31,7 @@ const {
 } = DATA_ELEMENTS;
 const usePncRules = () => {
   const [disabledFields, setDisabledFields] = useState([
-    SERVICES_TYPE,
+    // SERVICES_TYPE,
     HIGH_BLOOD_PRESSURE
   ]);
   const [hiddenFields, setHiddenFields] = useState([]);
@@ -58,23 +58,18 @@ const usePncRules = () => {
     let currentHiddenFields = [...hiddenFields];
     ///////////////////////////////////////////////////////////////////////////////////
     //Automatically set Service Type taken from ANC program stage
-    const foundEvent = currentEvents.find((ce) => {
-      const foundPs = ce.programStage === "IZ9GXqMAZV8";
-      const foundServiceType = ce.dataValues.find(
-        (dv) => dv.dataElement === SERVICES_TYPE && dv.value
-      );
-      return foundPs && foundServiceType;
-    });
-    if (foundEvent) {
-      const foundServiceType = foundEvent.dataValues.find(
-        (dv) => dv.dataElement === SERVICES_TYPE
-      );
-      changeDataValue(
-        currentEvent.event,
-        SERVICES_TYPE,
-        foundServiceType.value
-      );
+    if (!dataValues[SERVICES_TYPE]) {
+      const foundEvent = currentEvents.find((ce) => {
+        const foundPs = ce.programStage === "IZ9GXqMAZV8";
+        const foundServiceType = ce.dataValues.find((dv) => dv.dataElement === SERVICES_TYPE && dv.value);
+        return foundPs && foundServiceType;
+      });
+      if (foundEvent) {
+        const foundServiceType = foundEvent.dataValues.find((dv) => dv.dataElement === SERVICES_TYPE);
+        changeDataValue(currentEvent.event, SERVICES_TYPE, foundServiceType.value);
+      }
     }
+
     ///////////////////////////////////////////////////////////////////////////////////
     //HIDE PNC2_WITHIN_2_3DAYS IF PNC VISIT NUMBER = 1
     //HIDE PNC3_WITHIN_7_14DAYS IF PNC VISIT NUMBER = 1 or 2
@@ -84,38 +79,23 @@ const usePncRules = () => {
       currentHiddenFields.push(PNC2_WITHIN_2_3DAYS);
       currentHiddenFields.push(PNC3_WITHIN_7_14DAYS);
     } else {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== PNC2_WITHIN_2_3DAYS
-      );
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== PNC3_WITHIN_7_14DAYS
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== PNC2_WITHIN_2_3DAYS);
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== PNC3_WITHIN_7_14DAYS);
     }
     if (dataValues[PNC_VISIT_NUMBER] === "2") {
       currentHiddenFields.push(PNC3_WITHIN_7_14DAYS);
     } else {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== PNC3_WITHIN_7_14DAYS
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== PNC3_WITHIN_7_14DAYS);
     }
-    if (
-      dataValues[PNC_VISIT_NUMBER] &&
-      parseInt(dataValues[PNC_VISIT_NUMBER]) > 6
-    ) {
+    if (dataValues[PNC_VISIT_NUMBER] && parseInt(dataValues[PNC_VISIT_NUMBER]) > 6) {
       currentHiddenFields.push(PNC_IN_6WEEKS);
     } else {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== PNC_IN_6WEEKS
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== PNC_IN_6WEEKS);
     }
     ///////////////////////////////////////////////////////////////////////////////////
     //Set high blood pressure = "true" if systoloc > 140 or diastolic > 90
-    const diastolicBloodPressure = dataValues[DIASTOLIC_BLOOD_PRESSURE]
-      ? parseInt(dataValues[DIASTOLIC_BLOOD_PRESSURE])
-      : null;
-    const systolicBloodPressure = dataValues[SYSTOLIC_BLOOD_PRESSURE]
-      ? parseInt(dataValues[SYSTOLIC_BLOOD_PRESSURE])
-      : null;
+    const diastolicBloodPressure = dataValues[DIASTOLIC_BLOOD_PRESSURE] ? parseInt(dataValues[DIASTOLIC_BLOOD_PRESSURE]) : null;
+    const systolicBloodPressure = dataValues[SYSTOLIC_BLOOD_PRESSURE] ? parseInt(dataValues[SYSTOLIC_BLOOD_PRESSURE]) : null;
     if (diastolicBloodPressure || systolicBloodPressure) {
       if (diastolicBloodPressure > 90 || systolicBloodPressure > 140) {
         changeDataValue(currentEvent.event, HIGH_BLOOD_PRESSURE, "true");
@@ -128,17 +108,13 @@ const usePncRules = () => {
     ///////////////////////////////////////////////////////////////////////////////////
     //HIDE mother or baby abnormal condition if condition = normal
     if (dataValues[MOTHER_CONDITION] === "Abnormal") {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== ABNORMAL_MOTHER_CONDITION
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== ABNORMAL_MOTHER_CONDITION);
     } else {
       currentHiddenFields.push(ABNORMAL_MOTHER_CONDITION);
       changeDataValue(currentEvent.event, ABNORMAL_MOTHER_CONDITION, "");
     }
     if (dataValues[BABY_CONDITION] === "Abnormal") {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== ABNORMAL_BABY_CONDITION
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== ABNORMAL_BABY_CONDITION);
     } else {
       currentHiddenFields.push(ABNORMAL_BABY_CONDITION);
       changeDataValue(currentEvent.event, ABNORMAL_BABY_CONDITION, "");
@@ -146,12 +122,8 @@ const usePncRules = () => {
     ///////////////////////////////////////////////////////////////////////////////////
     //HIDE Breastfeeding section if breastfeeding = false
     if (dataValues[EXCLUSIVE_BREASTFEEDING] === "false") {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== REASON_NOT_BREASTFEEDING
-      );
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== WHAT_TO_FEED_BABY
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== REASON_NOT_BREASTFEEDING);
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== WHAT_TO_FEED_BABY);
     } else {
       currentHiddenFields.push(REASON_NOT_BREASTFEEDING);
       currentHiddenFields.push(WHAT_TO_FEED_BABY);
@@ -160,27 +132,18 @@ const usePncRules = () => {
     }
     ///////////////////////////////////////////////////////////////////////////////////
     //HIDE IFA section if received = false
-    if (
-      dataValues[RECEIVED_IFA_TABLETS] === "true" ||
-      dataValues[RECEIVED_IFA_TABLETS] === "false"
-    ) {
+    if (dataValues[RECEIVED_IFA_TABLETS] === "true" || dataValues[RECEIVED_IFA_TABLETS] === "false") {
       currentDisabledFields.push(RECEIVED_IFA_TABLETS);
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== HOW_MANY_IFA_TABLETS
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== HOW_MANY_IFA_TABLETS);
     } else {
       currentHiddenFields.push(HOW_MANY_IFA_TABLETS);
-      currentDisabledFields = currentDisabledFields.filter(
-        (cdf) => cdf !== RECEIVED_IFA_TABLETS
-      );
+      currentDisabledFields = currentDisabledFields.filter((cdf) => cdf !== RECEIVED_IFA_TABLETS);
       changeDataValue(currentEvent.event, HOW_MANY_IFA_TABLETS, "");
     }
     ///////////////////////////////////////////////////////////////////////////////////
     //HIDE Vitamin B1 section if received = false
     if (dataValues[RECEIVED_VITAMIN_B1] === "true") {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== HOW_MANY_VITAMIN_B1_TABLETS
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== HOW_MANY_VITAMIN_B1_TABLETS);
     } else {
       currentHiddenFields.push(HOW_MANY_VITAMIN_B1_TABLETS);
       changeDataValue(currentEvent.event, HOW_MANY_VITAMIN_B1_TABLETS, "");
@@ -191,16 +154,11 @@ const usePncRules = () => {
       currentHiddenFields.push(PNC1_WITHIN_24HRS);
       changeDataValue(currentEvent.event, PNC1_WITHIN_24HRS, "");
     } else {
-      currentHiddenFields = currentHiddenFields.filter(
-        (f) => f !== PNC1_WITHIN_24HRS
-      );
+      currentHiddenFields = currentHiddenFields.filter((f) => f !== PNC1_WITHIN_24HRS);
     }
 
     //
-    const currentEventDate =
-      currentEvent && currentEvent.eventDate
-        ? new Date(currentEvent.eventDate)
-        : new Date();
+    const currentEventDate = currentEvent && currentEvent.eventDate ? new Date(currentEvent.eventDate) : new Date();
     currentProps[NEXT_APPOINTMENT_DATE] = {
       maxDate: format(add(currentEventDate, { days: 90 }), "yyyy-MM-dd")
     };
@@ -211,15 +169,8 @@ const usePncRules = () => {
 
   useEffect(() => {
     if (previousPncEvents.length > 0) {
-      const foundReceivedIFA = findDataValue(
-        previousPncEvents[0].dataValues,
-        RECEIVED_IFA_TABLETS
-      );
-      changeDataValue(
-        currentEvent.event,
-        RECEIVED_IFA_TABLETS,
-        foundReceivedIFA
-      );
+      const foundReceivedIFA = findDataValue(previousPncEvents[0].dataValues, RECEIVED_IFA_TABLETS);
+      changeDataValue(currentEvent.event, RECEIVED_IFA_TABLETS, foundReceivedIFA);
     }
   }, [currentEvent ? currentEvent.event : ""]);
 
