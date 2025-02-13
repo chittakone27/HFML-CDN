ee685d6279e00f5ebd11be710c6b17af242bdc13
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2025-02-13 11:40:00 +0700

    push

--- src/configs/laotracker/program-forms/eir/GrowthMonitoring/GrowthMonitoring.jsx
+++ src/configs/laotracker/program-forms/eir/GrowthMonitoring/GrowthMonitoring.jsx
@@ -1,169 +1,3 @@
-<<<<<<< HEAD
-import { Box, Table, TableBody } from "@mui/material";
-import DataValueField from "@/ui/TrackerCapture/EventForm/DataValueFieldNoBlur";
-import DataValueLabel from "@/ui/TrackerCapture/EventForm/DataValueLabel";
-//
-import { useTranslation } from "react-i18next";
-import { useShallow } from "zustand/react/shallow";
-import useSelectionStore from "@/state/selection";
-import useGrowthMonitorRules from "./useGrowthMonitorRules";
-//
-import {
-  withEventDate,
-  RowMapper,
-  SectionCollapse
-} from "@/configs/lao/program-forms/common/tracker";
-//
-import {
-  GROWTH_MONITOR_ID,
-  CHILD_NUTRI_STATUS_SECTION_ID,
-  CHILD_SYMPS_TREAT_SECTION_ID,
-  MEDIC_SYMPTOM_IDS,
-  DISABLED_FIELDS,
-  sectionTitleStyling,
-  sectionCollapseStyling
-} from "./const";
-import { CHILD_NUTRI_SECTION_UI } from "./mapping";
-//
-import "./GrowthMonitoring.css";
-import "../eir.css";
-
-const GrowthMonitoring = () => {
-  const { t } = useTranslation();
-  const { program } = useSelectionStore(
-    useShallow((state) => ({
-      program: state.program
-    }))
-  );
-  const growthMonitorStage = program.programStages.find(
-    (progState) => progState.id === GROWTH_MONITOR_ID
-  );
-  const { programStageSections } = growthMonitorStage;
-  const { hiddenFields } = useGrowthMonitorRules();
-
-  const renderSectionContents = (pss, tableConfigs) => {
-    switch (pss.id) {
-      case CHILD_NUTRI_STATUS_SECTION_ID:
-        return (
-          <div className="child-nutri-container">
-            {CHILD_NUTRI_SECTION_UI.map((col) => {
-              return (
-                <SectionCollapse
-                  title={col["colTitle"]}
-                  sx={{ m: 0.5, width: "50%" }}
-                  disabledCollapse
-                >
-                  <Table>
-                    <TableBody>
-                      <RowMapper
-                        rows={col["colConfigs"]}
-                        tableName={col["colTitle"]}
-                        context="event"
-                      />
-                    </TableBody>
-                  </Table>
-                </SectionCollapse>
-              );
-            })}
-          </div>
-        );
-      case CHILD_SYMPS_TREAT_SECTION_ID:
-        // console.log(tableConfigs);
-        const tempNewConfigs = tableConfigs.filter(
-          (cellConfig) =>
-            cellConfig[0]["id"] === "SsTzQsXnzQB" ||
-            !MEDIC_SYMPTOM_IDS.includes(cellConfig[0]["id"])
-        );
-        const newConfigs = tempNewConfigs.map((oldConfig) => {
-          if (oldConfig[0]["id"] === "SsTzQsXnzQB") {
-            return [
-              { display: "text", text: t("medicSymps") },
-              {
-                customCellProp: {
-                  sx: {
-                    width: "78%"
-                  }
-                },
-                customCell: (
-                  <div className="medical-symps-container">
-                    {MEDIC_SYMPTOM_IDS.map((id) => {
-                      return (
-                        <div
-                          style={{
-                            display: "flex",
-                            alignItems: "center"
-                          }}
-                        >
-                          <DataValueField dataElement={id} />
-                          <DataValueLabel dataElement={id} />
-                        </div>
-                      );
-                    })}
-                  </div>
-                )
-              }
-            ];
-          } else {
-            return oldConfig;
-          }
-        });
-        console.log(newConfigs);
-        return (
-          <Table>
-            <TableBody>
-              <RowMapper
-                rows={newConfigs}
-                tableName={pss.displayName}
-                context="event"
-              />
-            </TableBody>
-          </Table>
-        );
-      default:
-        return (
-          <Table>
-            <TableBody>
-              <RowMapper
-                rows={tableConfigs}
-                tableName={pss.displayName}
-                context="event"
-              />
-            </TableBody>
-          </Table>
-        );
-    }
-  };
-
-  return (
-    <Box className="eir-form">
-      {programStageSections.map((pss) => {
-        const tableConfigs = pss.dataElements
-          .filter((de) => !hiddenFields.includes(de.id))
-          .map((de) => {
-            if (DISABLED_FIELDS.includes(de.id)) {
-              return [{ id: de.id, fieldProps: { disabled: true } }];
-            } else {
-              return [{ id: de.id }];
-            }
-          });
-        return (
-          <SectionCollapse
-            key={pss.id}
-            title={t(pss.displayName)}
-            titleStyling={sectionTitleStyling}
-            disabledCollapse
-            sx={sectionCollapseStyling}
-          >
-            {renderSectionContents(pss, tableConfigs)}
-          </SectionCollapse>
-        );
-      })}
-    </Box>
-  );
-};
-
-export default withEventDate(GrowthMonitoring, "eventDate");
-=======
 import { Box, Table, TableBody } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useShallow } from "zustand/react/shallow";
@@ -171,11 +5,7 @@ import useSelectionStore from "@/state/selection";
 import useGrowthMonitorRules from "./rules/useGrowthMonitorRules";
 import useDetailSectionRules from "./rules/useDetailSectionRules";
 import useChildNutritionStatusRules from "./rules/useChildNutritionStatusRules";
-import {
-  withEventDate,
-  RowMapper,
-  SectionCollapse
-} from "@/configs/lao/program-forms/common/tracker";
+import { withEventDate, RowMapper, SectionCollapse } from "@/configs/lao/program-forms/common/tracker";
 //
 import { GROWTH_MONITOR_ID, CHILD_NUTRI_STATUS_SECTION_ID } from "./const";
 import { CHILD_NUTRI_SECTION_UI } from "./mapping";
@@ -190,9 +20,7 @@ const GrowthMonitoring = () => {
       program: state.program
     }))
   );
-  const growthMonitorStage = program.programStages.find(
-    (progState) => progState.id === GROWTH_MONITOR_ID
-  );
+  const growthMonitorStage = program.programStages.find((progState) => progState.id === GROWTH_MONITOR_ID);
   const { programStageSections } = growthMonitorStage;
   // console.log(programStageSections);
   const { hiddenFields } = useGrowthMonitorRules();
@@ -204,9 +32,7 @@ const GrowthMonitoring = () => {
   return (
     <Box className="eir-growth-monitor-form">
       {programStageSections.map((pss) => {
-        const tableConfigs = pss.dataElements
-          .filter((de) => !hiddenFields.includes(de.id))
-          .map((de) => [{ id: de.id }]);
+        const tableConfigs = pss.dataElements.filter((de) => !hiddenFields.includes(de.id)).map((de) => [{ id: de.id }]);
         if (pss.id === CHILD_NUTRI_STATUS_SECTION_ID) {
           return (
             <SectionCollapse
@@ -232,18 +58,10 @@ const GrowthMonitoring = () => {
                   });
                   // console.log(finalConfigs);
                   return (
-                    <SectionCollapse
-                      title={col["colTitle"]}
-                      sx={{ m: 0.5, width: "50%" }}
-                      disabledCollapse
-                    >
+                    <SectionCollapse title={col["colTitle"]} sx={{ m: 0.5, width: "50%" }} disabledCollapse>
                       <Table sx={{ height: "auto" }}>
                         <TableBody>
-                          <RowMapper
-                            rows={finalConfigs}
-                            tableName={col["colTitle"]}
-                            context="event"
-                          />
+                          <RowMapper rows={finalConfigs} tableName={col["colTitle"]} context="event" />
                         </TableBody>
                       </Table>
                     </SectionCollapse>
@@ -264,11 +82,7 @@ const GrowthMonitoring = () => {
             >
               <Table>
                 <TableBody>
-                  <RowMapper
-                    rows={tableConfigs}
-                    tableName={pss.displayName}
-                    context="event"
-                  />
+                  <RowMapper rows={tableConfigs} tableName={pss.displayName} context="event" />
                 </TableBody>
               </Table>
             </SectionCollapse>
@@ -280,4 +94,3 @@ const GrowthMonitoring = () => {
 };
 
 export default withEventDate(GrowthMonitoring, "eventDate");
->>>>>>> eir-growth-monitor
--- src/configs/laotracker/program-forms/eir/GrowthMonitoring/rules/useGrowthMonitorRules.js
+++ src/configs/laotracker/program-forms/eir/GrowthMonitoring/rules/useGrowthMonitorRules.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD:src/configs/laotracker/program-forms/eir/GrowthMonitoring/useGrowthMonitorRules.js
-import { useEffect, useState } from "react";
-import useTrackerCaptureStore from "@/state/trackerCapture";
-import useCurrentEvent from "@/ui/TrackerCapture/EventForm/useCurrentEvent";
-import { useShallow } from "zustand/react/shallow";
-//
-import { differenceInWeeks, differenceInMonths } from "date-fns";
-//
-import { DOB_ATTR_ID } from "./const";
-
-const useGrowthMonitorRules = () => {
-  const [hiddenFields, setHiddenFields] = useState([]);
-  const { data, actions } = useTrackerCaptureStore(
-    useShallow((state) => ({
-      data: state.data,
-      actions: state.actions
-    }))
-  );
-  const { currentEvent } = useCurrentEvent();
-  const { changeDataValue } = actions;
-  const { currentTei } = data;
-
-  useEffect(() => {
-    let tempHiddenFields = [];
-    const dobObj = currentTei.attributes.find(
-      (attr) => attr["attribute"] === DOB_ATTR_ID
-    );
-    if (currentEvent.eventDate && dobObj && dobObj.value) {
-      const currEvtDate = new Date(currentEvent.eventDate);
-      const teiDob = new Date(dobObj.value);
-      const ageInWeeks = differenceInWeeks(currEvtDate, teiDob);
-      const ageInMonths = differenceInMonths(currEvtDate, teiDob);
-      changeDataValue(currentEvent.event, "DxOqZZgVQhF", ageInWeeks);
-      changeDataValue(currentEvent.event, "MV1yoC7BfnG", ageInMonths);
-      if (ageInMonths < 6 || ageInMonths > 11) {
-        tempHiddenFields.push("GVHTqqwolWD");
-      } else if (ageInMonths < 12 || ageInMonths > 59) {
-        tempHiddenFields.push("DzNWdRvRB11");
-      }
-      for (const hiddenField of tempHiddenFields) {
-        changeDataValue(currentEvent.event, hiddenField, "");
-      }
-      setHiddenFields([...tempHiddenFields]);
-    }
-  }, [JSON.stringify(currentEvent), JSON.stringify(currentTei)]);
-
-  return { hiddenFields: hiddenFields };
-};
-
-export default useGrowthMonitorRules;
-=======
 import { useEffect, useState } from "react";
 import useTrackerCaptureStore from "@/state/trackerCapture";
 import useCurrentEvent from "@/ui/TrackerCapture/EventForm/useCurrentEvent";
@@ -73,9 +22,7 @@ const useGrowthMonitorRules = () => {
   useEffect(() => {
     // console.log(currentEvent, currentTei);
     let tempHiddenFields = [];
-    const dobObj = currentTei.attributes.find(
-      (attr) => attr["attribute"] === DOB_ATTR_ID
-    );
+    const dobObj = currentTei.attributes.find((attr) => attr["attribute"] === DOB_ATTR_ID);
     if (currentEvent.eventDate && dobObj && dobObj.value) {
       const currEvtDate = new Date(currentEvent.eventDate);
       const teiDob = new Date(dobObj.value);
@@ -101,4 +48,3 @@ const useGrowthMonitorRules = () => {
 };
 
 export default useGrowthMonitorRules;
->>>>>>> eir-growth-monitor:src/configs/laotracker/program-forms/eir/GrowthMonitoring/rules/useGrowthMonitorRules.js
