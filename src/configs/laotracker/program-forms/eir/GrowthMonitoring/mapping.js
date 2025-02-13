ba2fe50f190749c5fe203eb47070907a6525bcf2
Uoc Trinh <zhenghengyue@gmail.com>
2025-02-13 09:59:58 +0700

    Small update

--- src/configs/laotracker/program-forms/eir/GrowthMonitoring/mapping.js
+++ src/configs/laotracker/program-forms/eir/GrowthMonitoring/mapping.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-export const CHILD_NUTRI_SECTION_UI = [
-  {
-    colTitle: "Report child status",
-    colConfigs: [
-      [{ id: "rWQgHzlRxnr" }],
-      [{ id: "nbjCuxRtsYm" }],
-      [{ id: "cMhs6WKOp5f" }]
-    ]
-  },
-  {
-    colTitle: "Correct child status",
-    colConfigs: [
-      [{ id: "eXgCO34QypV" }],
-      [{ id: "J2sDxcMfht1" }],
-      [{ id: "QppFJG84N4H" }]
-    ]
-  }
-];
-=======
 export const CHILD_NUTRI_SECTION_UI = [
   {
     colTitle: "Report child status",
@@ -5285,4 +5265,3 @@ export const GROWTH_MONITORING = {
     }
   }
 };
->>>>>>> eir-growth-monitor
