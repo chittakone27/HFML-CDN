b1b49df42494b4557a2e0680136f2bddcff50255
Anh Dũng Nguyễn <nguyenanhdung191@gmail.com>
2025-06-23 17:02:22 +0700

    push

--- src/ui/TrackerCapture/EventForm/DataValueFieldNoBlur.jsx
+++ src/ui/TrackerCapture/EventForm/DataValueFieldNoBlur.jsx
@@ -10,55 +10,39 @@ import ErrorDialog from "@/ui/common/ErrorDialog/ErrorDialog";
 import { event } from "@/api";
 import { pickTranslation } from "@/utils/utils";
 import { useTranslation } from "react-i18next";
+import _ from "lodash";
 const { saveEvent } = event;
 const { VITE_MODE } = import.meta.env;
 
 const DataValueFieldNoBlur = (props) => {
   const { i18n } = useTranslation();
   const [apiError, setApiError] = useState(null);
-  let {
-    label,
-    dataElement,
-    disabled,
-    focus,
-    hiddenOptions,
-    helpers,
-    customValueSet,
-  } = props;
+  let { label, dataElement, disabled, focus, hiddenOptions, helpers, customValueSet } = props;
   const { dataElements, optionSets } = useMetadataStore(
     (state) => ({
       dataElements: state.dataElements,
-      optionSets: state.optionSets,
+      optionSets: state.optionSets
     }),
     shallow
   );
   const orgUnit = useSelectionStore((state) => state.orgUnit);
   const { currentEvent, currentProgramStage } = useCurrentEvent();
-  const { actions, layout } = useTrackerCaptureStore(
-    (state) => ({ actions: state.actions, layout: state.layout }),
-    shallow
-  );
+  const { actions, layout } = useTrackerCaptureStore((state) => ({ actions: state.actions, layout: state.layout }), shallow);
   const { changeDataValue, changeEventProperty, setLayout } = actions;
-  const foundPde = currentProgramStage.programStageDataElements.find(
-    (pde) => pde.dataElement.id === dataElement
-  );
+  const foundPde = currentProgramStage.programStageDataElements.find((pde) => pde.dataElement.id === dataElement);
   const foundDe = dataElements.find((de) => de.id === dataElement);
   foundDe.mandatory = foundPde ? foundPde.compulsory : false;
   foundDe.allowFutureDate = foundPde.allowFutureDate;
-  const foundOptionSet = foundDe.optionSetValue
-    ? optionSets.find((os) => os.id === foundDe.optionSet.id)
-    : null;
+  const foundOptionSet = foundDe.optionSetValue ? optionSets.find((os) => os.id === foundDe.optionSet.id) : null;
   let valueSet = foundOptionSet
-    ? foundOptionSet.options.map((o) => ({
+    ? _.sortBy(foundOptionSet.options, "sortOrder").map((o) => ({
         label: pickTranslation(o, i18n.language, "name"),
-        value: o.code,
+        value: o.code
       }))
     : null;
   const { valueType, id } = foundDe;
   const completed = currentEvent.status === "COMPLETED" ? true : false;
-  const foundDataValue = currentEvent.dataValues.find(
-    (dv) => dv.dataElement === dataElement
-  );
+  const foundDataValue = currentEvent.dataValues.find((dv) => dv.dataElement === dataElement);
   const value = foundDataValue ? foundDataValue.value : "";
 
   if (hiddenOptions) {
@@ -89,10 +73,7 @@ const DataValueFieldNoBlur = (props) => {
     currentDisabled = true;
   }
 
-  if (
-    currentEvent.orgUnit !== orgUnit.id &&
-    currentEvent.status !== "SCHEDULE"
-  ) {
+  if (currentEvent.orgUnit !== orgUnit.id && currentEvent.status !== "SCHEDULE") {
     currentDisabled = true;
   }
 
@@ -115,16 +96,10 @@ const DataValueFieldNoBlur = (props) => {
         change={(value) => {
           changeDataValue(currentEvent.event, dataElement, value);
         }}
-        maxDate={
-          foundDe.allowFutureDate ? undefined : moment().format("YYYY-MM-DD")
-        }
-        initialDate={
-          foundDe.valueType === "AGE" ? currentEvent.eventDate : undefined
-        }
+        maxDate={foundDe.allowFutureDate ? undefined : moment().format("YYYY-MM-DD")}
+        initialDate={foundDe.valueType === "AGE" ? currentEvent.eventDate : undefined}
         accept={
-          foundDe.valueType === "DATE" ||
-          foundDe.valueType === "AGE" ||
-          foundDe.valueType === "ORGANISATION_UNIT"
+          foundDe.valueType === "DATE" || foundDe.valueType === "AGE" || foundDe.valueType === "ORGANISATION_UNIT"
             ? (value) => {
                 changeDataValue(currentEvent.event, dataElement, value);
               }
