c7f9e5cc2af636aa0f4a376ebf0ece23815093a6
Uoc Trinh <zhenghengyue@gmail.com>
2025-10-01 15:02:13 +0700

    fix(deli-registry): remove fields in PDF

--- src/configs/laotracker/utils/deli-registry-pdf-configs.js
+++ src/configs/laotracker/utils/deli-registry-pdf-configs.js
@@ -50,42 +50,42 @@ export const VALUE_COORDS_CONFIGS = {
       xMinusCoord: 450,
       yMinusCoord: 414
     },
-    {
-      ID: "xbwURy2jG2K",
-      valueType: "TEXT",
-      xMinusCoord: 404,
-      yMinusCoord: 414
-    },
-    {
-      ID: "UNiaP6Oz7Mv",
-      valueType: "ORG_UNIT",
-      xMinusCoord: 365,
-      yMinusCoord: 414
-      // titleCoords: {
-      //   xMinusCoord: 500,
-      //   yMinusCoord: 139
-      // }
-    },
-    {
-      ID: "oVwa5LfjnvA",
-      valueType: "ORG_UNIT",
-      xMinusCoord: 230,
-      yMinusCoord: 414
-      // titleCoords: {
-      //   xMinusCoord: 480,
-      //   yMinusCoord: 123
-      // }
-    },
-    {
-      ID: "r8bZppSsIvR",
-      valueType: "ORG_UNIT",
-      xMinusCoord: 556,
-      yMinusCoord: 428
-      // titleCoords: {
-      //   xMinusCoord: 460,
-      //   yMinusCoord: 107
-      // }
-    },
+    // {
+    //   ID: "xbwURy2jG2K",
+    //   valueType: "TEXT",
+    //   xMinusCoord: 404,
+    //   yMinusCoord: 414
+    // },
+    // {
+    //   ID: "UNiaP6Oz7Mv",
+    //   valueType: "ORG_UNIT",
+    //   xMinusCoord: 365,
+    //   yMinusCoord: 414
+    //   // titleCoords: {
+    //   //   xMinusCoord: 500,
+    //   //   yMinusCoord: 139
+    //   // }
+    // },
+    // {
+    //   ID: "oVwa5LfjnvA",
+    //   valueType: "ORG_UNIT",
+    //   xMinusCoord: 230,
+    //   yMinusCoord: 414
+    //   // titleCoords: {
+    //   //   xMinusCoord: 480,
+    //   //   yMinusCoord: 123
+    //   // }
+    // },
+    // {
+    //   ID: "r8bZppSsIvR",
+    //   valueType: "ORG_UNIT",
+    //   xMinusCoord: 556,
+    //   yMinusCoord: 428
+    //   // titleCoords: {
+    //   //   xMinusCoord: 460,
+    //   //   yMinusCoord: 107
+    //   // }
+    // },
     {
       ID: "UJioxXRLgpw",
       valueType: "OPTIONS",
@@ -109,80 +109,80 @@ export const VALUE_COORDS_CONFIGS = {
     //   xMinusCoord: 420,
     //   yMinusCoord: 328
     // },
-    {
-      ID: "tQeFLjYbqzv",
-      valueType: "DATE",
-      valueSrc: "ATTR",
-      dateCoords: {
-        yearCoord: {
-          xMinusCoord: 340,
-          yMinusCoord: 217
-        },
-        monthCoord: {
-          xMinusCoord: 360,
-          yMinusCoord: 217
-        },
-        dayCoord: {
-          xMinusCoord: 380,
-          yMinusCoord: 217
-        }
-      }
-    },
-    {
-      ID: "DmuazFb368B",
-      valueType: "SEX",
-      valueSrc: "ATTR",
-      maleCoord: {
-        xMinusCoord: 497,
-        yMinusCoord: 215
-      },
-      femaleCoord: {
-        xMinusCoord: 530,
-        yMinusCoord: 215
-      }
-    },
-    {
-      ID: "P1fhF8iYjm7",
-      valueType: "TEXT",
-      valueSrc: "DE",
-      xMinusCoord: 275,
-      yMinusCoord: 217
-    },
-    {
-      ID: "EsuDp7LMZTW",
-      valueType: "TEXT",
-      valueSrc: "DE",
-      xMinusCoord: 175,
-      yMinusCoord: 217
-    },
-    {
-      ID: "lWf6j4de9MW",
-      valueType: "TEXT",
-      valueSrc: "DE",
-      xMinusCoord: 185,
-      yMinusCoord: 200
-    },
-    {
-      ID: "CaHxfqvP1oP",
-      valueType: "TEXT",
-      valueSrc: "DE",
-      xMinusCoord: 115,
-      yMinusCoord: 200
-    },
-    {
-      ID: "MKkYDC0tUIr",
-      valueType: "TEXT",
-      valueSrc: "DE",
-      xMinusCoord: 420,
-      yMinusCoord: 200
-    },
-    {
-      ID: "JS8uvgPMJxl",
-      valueType: "TEXT",
-      valueSrc: "DE",
-      xMinusCoord: 350,
-      yMinusCoord: 200
-    }
+    // {
+    //   ID: "tQeFLjYbqzv",
+    //   valueType: "DATE",
+    //   valueSrc: "ATTR",
+    //   dateCoords: {
+    //     yearCoord: {
+    //       xMinusCoord: 340,
+    //       yMinusCoord: 217
+    //     },
+    //     monthCoord: {
+    //       xMinusCoord: 360,
+    //       yMinusCoord: 217
+    //     },
+    //     dayCoord: {
+    //       xMinusCoord: 380,
+    //       yMinusCoord: 217
+    //     }
+    //   }
+    // },
+    // {
+    //   ID: "DmuazFb368B",
+    //   valueType: "SEX",
+    //   valueSrc: "ATTR",
+    //   maleCoord: {
+    //     xMinusCoord: 497,
+    //     yMinusCoord: 215
+    //   },
+    //   femaleCoord: {
+    //     xMinusCoord: 530,
+    //     yMinusCoord: 215
+    //   }
+    // },
+    // {
+    //   ID: "P1fhF8iYjm7",
+    //   valueType: "TEXT",
+    //   valueSrc: "DE",
+    //   xMinusCoord: 275,
+    //   yMinusCoord: 217
+    // },
+    // {
+    //   ID: "EsuDp7LMZTW",
+    //   valueType: "TEXT",
+    //   valueSrc: "DE",
+    //   xMinusCoord: 175,
+    //   yMinusCoord: 217
+    // },
+    // {
+    //   ID: "lWf6j4de9MW",
+    //   valueType: "TEXT",
+    //   valueSrc: "DE",
+    //   xMinusCoord: 185,
+    //   yMinusCoord: 200
+    // },
+    // {
+    //   ID: "CaHxfqvP1oP",
+    //   valueType: "TEXT",
+    //   valueSrc: "DE",
+    //   xMinusCoord: 115,
+    //   yMinusCoord: 200
+    // },
+    // {
+    //   ID: "MKkYDC0tUIr",
+    //   valueType: "TEXT",
+    //   valueSrc: "DE",
+    //   xMinusCoord: 420,
+    //   yMinusCoord: 200
+    // },
+    // {
+    //   ID: "JS8uvgPMJxl",
+    //   valueType: "TEXT",
+    //   valueSrc: "DE",
+    //   xMinusCoord: 350,
+    //   yMinusCoord: 200
+    // }
     // {
     //   ID: "lmBw4l6o2O4",
     //   valueType: "TEXT",
